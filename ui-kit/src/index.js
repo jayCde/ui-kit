e02077d96e9d092a7eea16e09a85a895dc16730b
Jeff Asmah <jeff.k.asmah@outlook.com>
2021-11-03 18:21:33 +0000

    Modify index.js, use Suspense, react lazy

--- ui-kit/src/index.js
+++ ui-kit/src/index.js
@@ -1,14 +1,26 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React, { Suspense } from "react";
+import ReactDOM from "react-dom";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import "./index.css";
+import App from "./App";
+import test from "./components/test";
+import reportWebVitals from "./reportWebVitals";
+
+const PageLayout = React.lazy(() => import("./components/PageLayout"));
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Suspense>
+      <Router>
+        <Switch>
+          <Route path="/" exact component={App} />
+          <Router path="/kit" component={PageLayout}></Router>
+          <Route path="/hi" component={test}></Route>
+        </Switch>
+      </Router>
+    </Suspense>
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
 
 // If you want to start measuring performance in your app, pass a function
