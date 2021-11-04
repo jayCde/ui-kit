75ce81ec61e1ad4d30fc3cf2741715f890123e41
Jeff Asmah <jeff.k.asmah@outlook.com>
2021-11-04 06:40:12 +0000

    Modify main component

---
+++ ui-kit/src/components/kit.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import ItemsLayout from './itemsLayout';
+import SideNavigation from './Sidebar'
+
+function Uikit() {
+    return (
+        <div>
+            <SideNavigation/>
+            <ItemsLayout/>
+        </div>
+    )
+}
+
+export default Uikit
