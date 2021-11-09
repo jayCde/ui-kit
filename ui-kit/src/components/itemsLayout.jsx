622193e464ca526194f7fef43d40f2a6f8e1ad0d
Jeff Asmah <jeff.k.asmah@outlook.com>
2021-11-09 10:36:45 +0000

    Imports, conditionally render sub components

--- ui-kit/src/components/itemsLayout.jsx
+++ ui-kit/src/components/itemsLayout.jsx
@@ -1,12 +1,44 @@
 import React from "react";
 import "../styles/layout.css";
 import AvatarsComponent from "./items/avatars/Avatars";
+import ButtonComponent from "./items/buttons/Buttons";
+import CardsComponent from "./items/cards/Cards";
+import ChartsComponent from "./items/charts/Charts";
+import ContainersComponent from "./items/Containers/Containers";
+import DropdownsComponent from "./items/Dropdowns/dropdowns";
+import FormsComponent from "./items/Forms/forms";
+import InputsComponent from "./items/inputs/Inputs";
+import MenusComponent from "./items/menus/Menus";
+import ModalsComponent from "./items/Modals/Modals";
+import NavigationBarsComponent from "./items/navigationbars/NavigationBars";
+import NotificationsComponent from "./items/notifications/Notifications";
+import PaginationComponent from "./items/Pagination/pagination";
+import ProgressBarsComponent from "./items/progressbars/ProgressBars";
+import ScrollsComponent from "./items/scrolls/Scrolls";
+import SpinnersComponent from "./items/spinners/Spinners";
+import TablesComponent from "./items/tables/Tables";
 
-function ItemsLayout() {
+function ItemsLayout({showComponents}) {
   return (
     <div className="layoutContainer">
       <div className="layoutItems">
-        <AvatarsComponent/>
+        {showComponents.selectedAvatars && <AvatarsComponent/>}
+        {showComponents.setselectedButtons && <ButtonComponent/>}
+        {showComponents.selectedCards && <CardsComponent/>}
+        {showComponents.selectedCharts && <ChartsComponent/>}
+        {showComponents.selectedContainers && <ContainersComponent/>}
+        {showComponents.selectedDropdowns && <DropdownsComponent/>}
+        {showComponents.selectedForms && <FormsComponent/>}
+        {showComponents.selectedInputs && <InputsComponent/>}
+        {showComponents.selectedMenus && <MenusComponent/>}
+        {showComponents.selectedModals && <ModalsComponent/>}
+        {showComponents.selectedNavs && <NavigationBarsComponent/>}
+        {showComponents.selectedNotifications && <NotificationsComponent />}
+        {showComponents.selectedPagination && <PaginationComponent/>}
+        {showComponents.selectedProgresses && <ProgressBarsComponent/>}
+        {showComponents.selectedScrolls && <ScrollsComponent/>}
+        {showComponents.selectedSpinners && <SpinnersComponent/>}
+        {showComponents.selectedTables && <TablesComponent/>}
       </div>
     </div>
   );
