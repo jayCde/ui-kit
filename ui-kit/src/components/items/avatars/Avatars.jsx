4d96382c8cd85266a4010033830ddde0788705fe
Jeff Asmah <jeff.k.asmah@outlook.com>
2021-11-10 19:21:21 +0000

    Add duplicate elements to test layouts

--- ui-kit/src/components/items/avatars/Avatars.jsx
+++ ui-kit/src/components/items/avatars/Avatars.jsx
@@ -1,13 +1,43 @@
-import React from 'react'
+import React from 'react';
+import "../../../styles/items/avatars.css";
+import { avatarAssets } from '../../assets/assets';
 
 function AvatarsComponent() {
     return (
         <div className="avatarContainer">
-            <h4 className="avatarHeading">Layout here </h4>
+            <h4 className="avatarHeading">Avatars...</h4>
             <div className="avatarItems">
                 <div className="avatarItem">
-                    Some avatars...
+                    <div className="item">
+                        Avatar Here
+                    </div>
                 </div>
+                <div className="avatarItem">
+                    <div className="item">
+                        Avatar Here
+                    </div>
+                </div>
+                <div className="avatarItem">
+                    <div className="item">
+                        Avatar Here
+                    </div>
+                </div>
+                <div className="avatarItem">
+                    <div className="item">
+                        Avatar Here
+                    </div>
+                </div>
+                <div className="avatarItem">
+                    <div className="item">
+                        Avatar Here
+                    </div>
+                </div>
+                <div className="avatarItem">
+                    <div className="item">
+                        Avatar Here
+                    </div>
+                </div>
+                
             </div>
         </div>
     )
