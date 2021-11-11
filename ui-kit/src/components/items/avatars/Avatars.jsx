56b804db9261b594ba5e181b413107ca569af17c
Jeff Asmah <jeff.k.asmah@outlook.com>
2021-11-11 16:10:31 +0000

    map and render avatar icons, Add snippet button

--- ui-kit/src/components/items/avatars/Avatars.jsx
+++ ui-kit/src/components/items/avatars/Avatars.jsx
@@ -1,17 +1,24 @@
 import React from 'react';
 import "../../../styles/items/avatars.css";
 import { avatarAssets } from '../../assets/assets';
+import {FaCode} from "react-icons/fa"
+
+
+
+
 
 function AvatarsComponent() {
     return (
         <div className="avatarContainer">
-            <h4 className="avatarHeading">Avatars...</h4>
+            <h5 className="avatarHeading">Avatars...</h5>
             <div className="avatarItems">
                 {avatarAssets.map((avatar)=>
                     <div className="avatarItem" key={avatar.id}>
                         <div className="item">
+                            <img src={avatar.resource} alt="asset-item"/>
                             {avatar.name}
                         </div>
+                        <button className="snippetBtn"><FaCode/></button>
                     </div>
                 )}
             </div>
