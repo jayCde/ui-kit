d03477c51152ed32b6f21b2da6566db5c6a09591
Jeff Asmah <jeff.k.asmah@outlook.com>
2021-11-11 11:31:18 +0000

    Add base asset objects

---
+++ ui-kit/src/components/assets/assets.js
@@ -0,0 +1,94 @@
+const avatarAssets = [
+    {
+        id: 1, 
+        name: "asset-1",
+        resource: "../../assets/avatars/iconmonstr-generation-2.svg"
+    },
+    {
+        id: 2, 
+        name: "asset-1",
+        resource: "../../assets/avatars/iconmonstr-generation-2.svg"
+    },
+    {
+        id: 3, 
+        name: "asset-1",
+        resource: "../../assets/avatars/iconmonstr-generation-2.svg"
+    },
+    {
+        id: 4, 
+        name: "asset-1",
+        resource: "../../assets/avatars/iconmonstr-generation-2.svg"
+    },
+    {
+        id: 5, 
+        name: "asset-1",
+        resource: "../../assets/avatars/iconmonstr-generation-2.svg"
+    },
+    {
+        id: 6, 
+        name: "asset-1",
+        resource: "../../assets/avatars/iconmonstr-generation-2.svg"
+    },
+    {
+        id: 7, 
+        name: "asset-1",
+        resource: "../../assets/avatars/iconmonstr-generation-2.svg"
+    },
+    {
+        id: 8, 
+        name: "asset-1",
+        resource: "../../assets/avatars/iconmonstr-generation-2.svg"
+    },
+    {
+        id: 9, 
+        name: "asset-1",
+        resource: "../../assets/avatars/iconmonstr-generation-2.svg"
+    }
+]
+
+const buttonAssets = [
+    {}
+]
+
+const cardsAssets = [
+    {}
+]
+
+const chartsAssets = [
+    {}
+]
+
+const containerAssets = [
+    {}
+]
+
+const dropdownAssets = [
+    {}
+    
+]
+
+const formsAssets = [
+    {}
+]
+
+const inputsAssets = [
+    {}
+]
+
+const menusAssets = [
+    {}
+]
+
+const navAssets = [
+    {}
+]
+
+
+
+
+
+
+
+
+
+export {avatarAssets, buttonAssets, cardsAssets};
