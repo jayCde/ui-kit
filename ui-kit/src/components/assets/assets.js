f0c7f6c05354a7a8a7cdae8309c9d5c3d2aba5e6
Jeff Asmah <jeff.k.asmah@outlook.com>
2021-11-11 16:08:52 +0000

    Modify avatarassets values

--- ui-kit/src/components/assets/assets.js
+++ ui-kit/src/components/assets/assets.js
@@ -1,49 +1,95 @@
+
 const avatarAssets = [
     {
         id: 1, 
         name: "asset-1",
-        resource: "../../assets/avatars/iconmonstr-generation-2.svg"
+        resource: "/avatars/iconmonstr-generation-2.svg"
     },
     {
         id: 2, 
-        name: "asset-1",
-        resource: "../../assets/avatars/iconmonstr-generation-2.svg"
+        name: "asset-2",
+        resource: "/avatars/iconmonstr-generation-4.svg"
     },
     {
         id: 3, 
-        name: "asset-1",
-        resource: "../../assets/avatars/iconmonstr-generation-2.svg"
+        name: "asset-3",
+        resource: "/avatars/iconmonstr-generation-5.svg"
     },
     {
         id: 4, 
-        name: "asset-1",
-        resource: "../../assets/avatars/iconmonstr-generation-2.svg"
+        name: "asset-4",
+        resource: "/avatars/iconmonstr-generation-7.svg"
     },
     {
         id: 5, 
-        name: "asset-1",
-        resource: "../../assets/avatars/iconmonstr-generation-2.svg"
+        name: "asset-5",
+        resource: "/avatars/iconmonstr-generation-8.svg"
     },
     {
         id: 6, 
-        name: "asset-1",
-        resource: "../../assets/avatars/iconmonstr-generation-2.svg"
+        name: "asset-6",
+        resource: "/avatars/iconmonstr-generation-11.svg"
     },
     {
         id: 7, 
-        name: "asset-1",
-        resource: "../../assets/avatars/iconmonstr-generation-2.svg"
+        name: "asset-7",
+        resource: "/avatars/iconmonstr-generation-12.svg"
     },
     {
         id: 8, 
-        name: "asset-1",
-        resource: "../../assets/avatars/iconmonstr-generation-2.svg"
+        name: "asset-8",
+        resource: "/avatars/iconmonstr-generation-13.svg"
     },
     {
         id: 9, 
-        name: "asset-1",
-        resource: "../../assets/avatars/iconmonstr-generation-2.svg"
-    }
+        name: "asset-9",
+        resource: "/avatars/avatar-svgrepo-com.svg"
+    },
+    {
+        id: 10, 
+        name: "asset-10",
+        resource: "/avatars/avatar-svgrepo-com(1).svg"
+    },
+    {
+        id: 11, 
+        name: "asset-11",
+        resource: "/avatars/avatar-svgrepo-com(2).svg"
+    },
+    {
+        id: 12, 
+        name: "asset-12",
+        resource: "/avatars/avatar-svgrepo-com(3).svg"
+    },
+    {
+        id: 13, 
+        name: "asset-13",
+        resource: "/avatars/avatar-svgrepo-com(4).svg"
+    },
+    {
+        id: 14, 
+        name: "asset-14",
+        resource: "/avatars/avatar-svgrepo-com(5).svg"
+    },
+    {
+        id: 15, 
+        name: "asset-15",
+        resource: "/avatars/avatar-svgrepo-com(6).svg"
+    },
+    {
+        id: 16, 
+        name: "asset-16",
+        resource: "/avatars/avatar-svgrepo-com(7).svg"
+    },
+    {
+        id: 17, 
+        name: "asset-17",
+        resource: "/avatars/avatar-svgrepo-com(8).svg"
+    },
+    {
+        id: 18, 
+        name: "asset-18",
+        resource: "/avatars/iconmonstr-school-23.svg"
+    },
 ]
 
 const buttonAssets = [
