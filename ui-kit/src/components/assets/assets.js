368967f49a9584b38a0c4b77afc467092bb5619a
Jeff Asmah <jeff.k.asmah@outlook.com>
2021-11-16 18:23:30 +0000

    Modify asset data

--- ui-kit/src/components/assets/assets.js
+++ ui-kit/src/components/assets/assets.js
@@ -1,140 +1,192 @@
+import { FaCode } from "react-icons/fa";
 
 const avatarAssets = [
-    {
-        id: 1, 
-        name: "asset-1",
-        resource: "/avatars/iconmonstr-generation-2.svg"
-    },
-    {
-        id: 2, 
-        name: "asset-2",
-        resource: "/avatars/iconmonstr-generation-4.svg"
-    },
-    {
-        id: 3, 
-        name: "asset-3",
-        resource: "/avatars/iconmonstr-generation-5.svg"
-    },
-    {
-        id: 4, 
-        name: "asset-4",
-        resource: "/avatars/iconmonstr-generation-7.svg"
-    },
-    {
-        id: 5, 
-        name: "asset-5",
-        resource: "/avatars/iconmonstr-generation-8.svg"
-    },
-    {
-        id: 6, 
-        name: "asset-6",
-        resource: "/avatars/iconmonstr-generation-11.svg"
-    },
-    {
-        id: 7, 
-        name: "asset-7",
-        resource: "/avatars/iconmonstr-generation-12.svg"
-    },
-    {
-        id: 8, 
-        name: "asset-8",
-        resource: "/avatars/iconmonstr-generation-13.svg"
-    },
-    {
-        id: 9, 
-        name: "asset-9",
-        resource: "/avatars/avatar-svgrepo-com.svg"
-    },
-    {
-        id: 10, 
-        name: "asset-10",
-        resource: "/avatars/avatar-svgrepo-com(1).svg"
-    },
-    {
-        id: 11, 
-        name: "asset-11",
-        resource: "/avatars/avatar-svgrepo-com(2).svg"
-    },
-    {
-        id: 12, 
-        name: "asset-12",
-        resource: "/avatars/avatar-svgrepo-com(3).svg"
-    },
-    {
-        id: 13, 
-        name: "asset-13",
-        resource: "/avatars/avatar-svgrepo-com(4).svg"
-    },
-    {
-        id: 14, 
-        name: "asset-14",
-        resource: "/avatars/avatar-svgrepo-com(5).svg"
-    },
-    {
-        id: 15, 
-        name: "asset-15",
-        resource: "/avatars/avatar-svgrepo-com(6).svg"
-    },
-    {
-        id: 16, 
-        name: "asset-16",
-        resource: "/avatars/avatar-svgrepo-com(7).svg"
-    },
-    {
-        id: 17, 
-        name: "asset-17",
-        resource: "/avatars/avatar-svgrepo-com(8).svg"
-    },
-    {
-        id: 18, 
-        name: "asset-18",
-        resource: "/avatars/iconmonstr-school-23.svg"
-    },
-]
+  {
+    id: 1,
+    name: "avatar-1",
+    resource: "/avatars/iconmonstr-generation-2.svg",
+    snippet: (
+        <blockquote>
+            <pre>
+            <code>
+                <div className="avatarItem">
+                <div className="item">
+                    <img src="" alt="asset-item" />
+                    avatar-1
+                </div>
+                <button className="snippetBtn">
+                    <FaCode />
+                </button>
+                </div>
+            </code>
+            </pre>
+        </blockquote>
+    ),
+  },
+  {
+    id: 2,
+    name: "avatar-2",
+    resource: "/avatars/iconmonstr-generation-4.svg",
+  },
+  {
+    id: 3,
+    name: "avatar-3",
+    resource: "/avatars/iconmonstr-generation-5.svg",
+  },
+  {
+    id: 4,
+    name: "avatar-4",
+    resource: "/avatars/iconmonstr-generation-7.svg",
+  },
+  {
+    id: 5,
+    name: "avatar-5",
+    resource: "/avatars/iconmonstr-generation-8.svg",
+  },
+  {
+    id: 6,
+    name: "avatar-6",
+    resource: "/avatars/iconmonstr-generation-11.svg",
+  },
+  {
+    id: 7,
+    name: "avatar-7",
+    resource: "/avatars/iconmonstr-generation-12.svg",
+  },
+  {
+    id: 8,
+    name: "avatar-8",
+    resource: "/avatars/iconmonstr-generation-13.svg",
+  },
+  {
+    id: 9,
+    name: "avatar-9",
+    resource: "/avatars/avatar-svgrepo-com.svg",
+  },
+  {
+    id: 10,
+    name: "avatar-10",
+    resource: "/avatars/avatar-svgrepo-com(1).svg",
+  },
+  {
+    id: 11,
+    name: "asset-11",
+    resource: "/avatars/avatar-svgrepo-com(2).svg",
+  },
+  {
+    id: 12,
+    name: "asset-12",
+    resource: "/avatars/avatar-svgrepo-com(3).svg",
+  },
+  {
+    id: 13,
+    name: "asset-13",
+    resource: "/avatars/avatar-svgrepo-com(4).svg",
+  },
+  {
+    id: 14,
+    name: "asset-14",
+    resource: "/avatars/avatar-svgrepo-com(5).svg",
+  },
+  {
+    id: 15,
+    name: "asset-15",
+    resource: "/avatars/avatar-svgrepo-com(6).svg",
+  },
+  {
+    id: 16,
+    name: "avatar-16",
+    resource: "/avatars/avatar-svgrepo-com(7).svg",
+  },
+  {
+    id: 17,
+    name: "avatar-17",
+    resource: "/avatars/avatar-svgrepo-com(8).svg",
+  },
+  {
+    id: 18,
+    name: "avatar-18",
+    resource: "/avatars/iconmonstr-school-23.svg",
+  },
+];
 
 const buttonAssets = [
-    {}
-]
+  {
+    id: 1,
+    name: "generic",
+    type: "generic"
+  },
+  {
+    id: 2,
+    name: "primary",
+    type: "primary"
+  },
+  {
+    id: 3,
+    name: "rounded",
+    type: "rounded"
+  },
+  {
+    id: 4,
+    name: "fully rounded",
+    type: "fullyRounded"
+  },
+  {
+    id: 5,
+    name: "animated - 1",
+    type: "animatedA"
+  },
+  {
+    id: 6,
+    name: "animated - 2",
+    type: "animatedB"
+  },
+  {
+    id: 7,
+    name: "animated - 3",
+    type: "animatedC"
+  },
+  {
+    id: 8,
+    name: "animated - 4",
+    type: "animatedD"
+  },
+  {
+    id: 9,
+    name: "styled - 1",
+    type: "styledA"
+  },
+  {
+    id: 10,
+    name: "styled - 2",
+    type: "styledB"
+  },
+  {
+    id: 11,
+    name: "styled - 3",
+    type: "styledC"
+  },
+  {
+    id: 12,
+    name: "styled -4",
+    type: "styledD"
+  }
+];
 
-const cardsAssets = [
-    {}
-]
+const cardsAssets = [{}];
 
-const chartsAssets = [
-    {}
-]
+const chartsAssets = [{}];
 
-const containerAssets = [
-    {}
-]
+const containerAssets = [{}];
 
-const dropdownAssets = [
-    {}
-    
-]
+const dropdownAssets = [{}];
 
-const formsAssets = [
-    {}
-]
+const formsAssets = [{}];
 
-const inputsAssets = [
-    {}
-]
+const inputsAssets = [{}];
 
-const menusAssets = [
-    {}
-]
+const menusAssets = [{}];
 
-const navAssets = [
-    {}
-]
+const navAssets = [{}];
 
-
-
-
-
-
-
-
-
-export {avatarAssets, buttonAssets, cardsAssets};
+export { avatarAssets, buttonAssets, cardsAssets };
